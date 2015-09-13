3c5e8b2b65bf20b69021a3443604652221f3ff1e
Ping <d79317@gmail.com>
2015-09-13 15:38:45 +0800

    first commit

---
+++ reservation3.js
@@ -0,0 +1,10 @@
+<html> 
+<head> 
+<title>JSPいゅ矪瞶</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312"> 
+</head> 
+
+<body> 
+<%=request.getParameter("name")%> 
+</body> 
+</html> 
---
+++ reservation3.jsp
@@ -0,0 +1,10 @@
+<html> 
+<head> 
+<title>JSP中文亂馬</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+</head> 
+
+<body> 
+<%=request.getParameter("name")%> 
+</body> 
+</html> 
